fd10add323d1595c7e1c82035e2c455a15ecf529
unknown <abodyalex1@gmail.com>
2022-09-15 00:28:59 +0300

    Add likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_191434) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_212804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_191434) do
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["users_id"], name: "index_likes_on_users_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -43,5 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_191434) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "users_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
