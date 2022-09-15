2c8d1c547cf5ce0446aaaba70db1837eb887e04e
unknown <abodyalex1@gmail.com>
2022-09-15 16:58:29 +0300

    Add associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_212804) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_125121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_212804) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
+    t.bigint "users_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["users_id"], name: "index_comments_on_users_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -52,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_212804) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "users_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "users_id"
   add_foreign_key "posts", "users", column: "author_id"
